510a9aab58a258a623a54b9270278305eb79d091
tealtan <allen@tanmade.com>
2013-06-14 20:17:04 -0400

    Generate Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GlossServer::Application.config.secret_key_base = '7dee2ee4bf5ebece91eaf0def4e139ab2395c801682573be62f3986b1908e30d5cc8e917ccf1192e359323782875aee442e7c166f60080502f4985ef20e5b41f'
