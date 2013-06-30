ccb85ea39733e80c66b6708c4a25fbc1cb5e5367
tealtan <allen@tanmade.com>
2013-06-30 00:00:39 -0400

    Add unique auto-generation of usernames from emails.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130615202652) do
+ActiveRecord::Schema.define(version: 20130630022905) do
 
   create_table "highlights", force: true do |t|
     t.text     "selected_text",    limit: 255
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20130615202652) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "authentication_token"
+    t.string   "username"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true
