deb83e4bd032b3b28fcc3510a08cdbf725b97b22
tealtan <allen@tanmade.com>
2013-07-01 19:41:30 -0400

    Properly show selection in highlights.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def combine_highlight(selected_text, surrounding_text)
+    if surrounding_text.include? selected_text
+      split_text_array = surrounding_text.split(selected_text)
+      content_tag(:p, split_text_array[0].html_safe + content_tag(:span, selected_text, class:"selected") + split_text_array[1].html_safe)
+    else
+      content_tag(:p, surrounding_text, class:"selected")
+    end
+  end
+
 end
