473858e4b6e3090575f695bc223c3e3d4cdcf80a
tealtan <allen@tanmade.com>
2013-07-02 00:15:25 -0400

    Refactor combine_highlight helper to be non-janky, courtesy of Aria Stewart.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,21 +2,8 @@ module ApplicationHelper
 
   def combine_highlight(selected_text, surrounding_text)
     if surrounding_text.include? selected_text
-      split_text_array = surrounding_text.split(selected_text)
-
-      if split_text_array[0].blank?
-        if split_text_array[1].blank?
-          content_tag(:p, content_tag(:span, selected_text, class:"selected"))
-        else
-          content_tag(:p, content_tag(:span, selected_text, class:"selected") + split_text_array[1].html_safe)
-        end        
-      else
-        if split_text_array[1].blank?
-          content_tag(:p, split_text_array[0].html_safe + content_tag(:span, selected_text, class:"selected"))
-        else
-          content_tag(:p, split_text_array[0].html_safe + content_tag(:span, selected_text, class:"selected") + split_text_array[1].html_safe)
-        end
-      end
+      split_text = surrounding_text.split(selected_text)
+      content_tag(:p, (split_text[0] || '').html_safe + content_tag(:span, selected_text, class:"selected") + (split_text[1] || '').html_safe)
     else
       content_tag(:p, surrounding_text, class:"selected")
     end
