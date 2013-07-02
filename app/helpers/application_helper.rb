9202b8bf749fd5275208997b006becc84d4ca563
tealtan <allen@tanmade.com>
2013-07-02 00:34:07 -0400

    Even more elegant refactor of combine_highlight helper, courtesy of Greg Leppert.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def combine_highlight(selected_text, surrounding_text)
     if surrounding_text.include? selected_text
       split_text = surrounding_text.split(selected_text)
-      content_tag(:p, (split_text[0] || '').html_safe + content_tag(:span, selected_text, class:"selected") + (split_text[1] || '').html_safe)
+      content_tag(:p, split_text[0].to_s.html_safe + content_tag(:span, selected_text, class:"selected") + split_text[1].to_s.html_safe)
     else
       content_tag(:p, surrounding_text, class:"selected")
     end
