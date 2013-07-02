d9e4a30f75bce8e4e142061ee14a78660a183975
tealtan <allen@tanmade.com>
2013-07-01 21:20:20 -0400

    Fix bug with unique username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  before_save :ensure_authentication_token, :ensure_unique_username
+  before_create :ensure_unique_username
+  before_save :ensure_authentication_token
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -7,11 +8,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
 
   def ensure_unique_username
-    if self.username.blank? || User.where(username: self.username).count > 1
+    if self.username.blank? || User.where(username: self.username).count > 0
       username_part = self.email.split("@").first
-      new_username = username_part.dup 
+      new_username = username_part.dup
       num = 2
-      while(User.where(username: new_username).count > 1)
+      while(User.where(username: new_username).count > 0)
         new_username = "#{username_part}#{num}"
         num += 1
       end
