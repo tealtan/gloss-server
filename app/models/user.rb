29644caf93bffeafa00479ec850d2a90731e5c67
Allen Tan <tealtan@Allens-iMac.local>
2014-02-10 00:21:26 -0500

    Update user registration to have username (instead of automatically generating one from email).

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,8 @@
 class User < ActiveRecord::Base
-  before_save :ensure_authentication_token, :ensure_unique_username
+  before_save :ensure_authentication_token
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
-
-  def ensure_unique_username
-    if self.username.blank? || User.where(username: self.username).count > 1
-      username_part = self.email.split("@").first
-      new_username = username_part.dup
-      num = 2
-      while(User.where(username: new_username).count > 0)
-        new_username = "#{username_part}#{num}"
-        num += 1
-      end
-      self.username = new_username
-    end
-  end
 end
--- lib/user_sanitizer.rb
+++ lib/user_sanitizer.rb
@@ -1,6 +1,10 @@
 class User::ParameterSanitizer < Devise::ParameterSanitizer
     private
+    def sign_up
+      default_params.permit(:username, :email, :password)
+    end
+
     def account_update
-        default_params.permit(:username, :email, :password, :password_confirmation, :current_password)
+      default_params.permit(:username, :email, :password, :password_confirmation, :current_password)
     end
 end
